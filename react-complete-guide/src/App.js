e70e8b0a2d31cccedec120f3ac252b5479b4caaa
Jan <Jan.Dobler@gmx.de>
2018-07-04 08:42:12 +0200

    Working with state and adding ecents including handlers and manipulating state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,15 +11,34 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // this.settate.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28},
+        { name: 'Jan', age: 26},
+        { name: 'Laura', age: 96}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Coding</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Maximillian!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')} >My Hobbies: Coding</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'},
