ce96df3b9de979d4d5cf350f4b03c9b5d7adae1f
Jan <Jan.Dobler@gmx.de>
2018-07-09 23:25:50 +0200

    adding the styling from before with CSS modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -45,16 +45,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -70,7 +63,8 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
+
     }
 
     const assignedClasses = [];
@@ -86,7 +80,7 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
           <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Person</button>
           {persons}
         </div>
